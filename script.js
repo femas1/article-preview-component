5c3dc86a7c51096ae9e628ed052ee44bf703c49b
femas1 <federico.massara2@gmail.com>
2022-10-11 07:49:35 +0200

    js logic for hover

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const shareBtn = document.querySelector('.user__share');
 const sharePopup = document.querySelector('.share-popup');
 
 shareBtn.addEventListener('mouseenter', () => {
-    shareBtn.style.backgroundColor = "hsl(214, 17%, 51%)"
+    shareBtn.classList.add('filter-white')
+    
+})
+shareBtn.addEventListener('mouseleave', () => {
+    shareBtn.classList.remove('filter-white')
     
 })
