24571edd0eff3e302a14527f6aa006630f9c469e
femas1 <federico.massara2@gmail.com>
2022-10-13 18:15:00 +0200

    adjusted js event listener popup

--- script.js
+++ script.js
@@ -2,28 +2,11 @@ const shareBtn = document.querySelector('.user__share');
 const sharePopup = document.querySelector('.share-popup');
 const sharePopupBackground = document.querySelector('.popupBackground')
 
-shareBtn.addEventListener('mouseenter', () => {
-    shareBtn.classList.add('filter-white');
-    sharePopupBackground.classList.remove('hidden');
-    sharePopup.classList.remove('hidden');
-    
-})
-shareBtn.addEventListener('mouseleave', () => {
-    shareBtn.classList.remove('filter-white');
-    sharePopupBackground.classList.add('hidden');
-    sharePopup.classList.add('hidden');
-    
+shareBtn.addEventListener('mouseover', () => {
+    shareBtn.classList.toggle('filter-white');  
 })
 
-sharePopupBackground.addEventListener('mouseenter', () => {
-    shareBtn.classList.add('filter-white');
-    sharePopupBackground.classList.remove('hidden');
-    sharePopup.classList.remove('hidden');
-    
-})
-
-sharePopup.addEventListener('mouseleave', () => {
-    shareBtn.classList.remove('filter-white');
-    sharePopupBackground.classList.add('hidden');
-    sharePopup.classList.add('hidden');
+shareBtn.addEventListener('click', () => {
+    sharePopupBackground.classList.toggle('hidden');
+    sharePopup.classList.toggle('hidden'); 
 })
